0b43c64d5f9103a6ba6972c04c391f98957ad71a
James Robert Perih <james@hotdang.ca>
2016-11-29 10:31:15 -0600

    Made a change

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
 
-var Examples = (props) => <h3>Examples component (super-stateless)</h3>;
+var Examples = (props) => <h3>Examples component (super-stateless)!</h3>;
 
 module.exports = Examples;
