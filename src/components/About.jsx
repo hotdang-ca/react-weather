044071421cf10a0edb3ab259515d10b697e2c189
James Robert Perih <james@hotdang.ca>
2016-11-29 12:35:45 -0600

    About Page Styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,20 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">About</h1>
+      <p>Simple weather application build on React. Built with <a href="https://www.udemy.com/the-complete-react-web-app-developer-course/">The Complete React Web App Developer Course</a>, by <strong>Andrew Mead</strong></p>
+      <div class="text-center flex-video widescreen">
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/N0_T9ZdZ7yk" allowfullscreen></iframe>
+      </div>
+      <p>Here are some of the tools I used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">OpenWeatherMap</a> - This was the weather data used.
+        </li>
+      </ul>
     </div>
   );
 };
